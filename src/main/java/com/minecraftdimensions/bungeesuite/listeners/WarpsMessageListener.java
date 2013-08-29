a9fbfbcdb7827304e61c94f33fc0a19740d4e180
bloodsplat <pure__instinct@hotmail.com>
2013-08-29 16:52:53 +1000

    Add bypass for perserver warps

--- src/main/java/com/minecraftdimensions/bungeesuite/listeners/WarpsMessageListener.java
+++ src/main/java/com/minecraftdimensions/bungeesuite/listeners/WarpsMessageListener.java
@@ -1,57 +1,54 @@
 package com.minecraftdimensions.bungeesuite.listeners;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.sql.SQLException;
-
 import com.minecraftdimensions.bungeesuite.managers.PlayerManager;
 import com.minecraftdimensions.bungeesuite.managers.WarpsManager;
 import com.minecraftdimensions.bungeesuite.objects.Location;
-
 import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
-public class WarpsMessageListener implements Listener {
-	
-	@EventHandler
-	public void receivePluginMessage(PluginMessageEvent event)
-			throws IOException, SQLException {
-		if(event.isCancelled()){
-			return;
-		}
-		if (!event.getTag().equalsIgnoreCase("BSWarps")) {
-			return;
-		}
-		event.setCancelled(true);
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
 
-		DataInputStream in = new DataInputStream(new ByteArrayInputStream(
-				event.getData()));
-		
-		String task = in.readUTF();
-		
-		if(task.equals("WarpPlayer")){
-			WarpsManager.sendPlayerToWarp(in.readUTF(),	in.readUTF(), in.readUTF(), in.readBoolean(), false);
-			return;
-		}
-		
-		if(task.equals("GetWarpsList")){
-			WarpsManager.getWarpsList(in.readUTF(),in.readBoolean(),in.readBoolean(),in.readBoolean());
-			return;
-		}
-		
-		if(task.equals("SetWarp")){
-			WarpsManager.setWarp(PlayerManager.getPlayer(in.readUTF()), in.readUTF(), new Location(((Server)event.getSender()).getInfo().getName(),in.readUTF(),in.readDouble(),in.readDouble(),in.readDouble(),in.readFloat(), in.readFloat()), in.readBoolean(),in.readBoolean());
-			return;
-		}
-		
-		if(task.equals("DeleteWarp")){
-			WarpsManager.deleteWarp(PlayerManager.getPlayer(in.readUTF()), in.readUTF());
-			return;
-		}
-		
-	}
+public class WarpsMessageListener implements Listener {
+
+    @EventHandler
+    public void receivePluginMessage( PluginMessageEvent event ) throws IOException, SQLException {
+        if ( event.isCancelled() ) {
+            return;
+        }
+        if ( !event.getTag().equalsIgnoreCase( "BSWarps" ) ) {
+            return;
+        }
+        event.setCancelled( true );
+
+        DataInputStream in = new DataInputStream( new ByteArrayInputStream( event.getData() ) );
+
+        String task = in.readUTF();
+
+        if ( task.equals( "WarpPlayer" ) ) {
+            WarpsManager.sendPlayerToWarp( in.readUTF(), in.readUTF(), in.readUTF(), in.readBoolean(), in.readBoolean() );
+            return;
+        }
+
+        if ( task.equals( "GetWarpsList" ) ) {
+            WarpsManager.getWarpsList( in.readUTF(), in.readBoolean(), in.readBoolean(), in.readBoolean() );
+            return;
+        }
+
+        if ( task.equals( "SetWarp" ) ) {
+            WarpsManager.setWarp( PlayerManager.getPlayer( in.readUTF() ), in.readUTF(), new Location( ( ( Server ) event.getSender() ).getInfo().getName(), in.readUTF(), in.readDouble(), in.readDouble(), in.readDouble(), in.readFloat(), in.readFloat() ), in.readBoolean(), in.readBoolean() );
+            return;
+        }
+
+        if ( task.equals( "DeleteWarp" ) ) {
+            WarpsManager.deleteWarp( PlayerManager.getPlayer( in.readUTF() ), in.readUTF() );
+            return;
+        }
+
+    }
 
 }
