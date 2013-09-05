5c51494bdf871c26f4812a08b79f06cd8d648414
bloodsplat <pure__instinct@hotmail.com>
2013-09-05 22:33:30 +1000

    Change the way affixes are sent and loaded by the server

---
+++ src/main/java/com/minecraftdimensions/bungeesuite/managers/Affix.java
@@ -0,0 +1,18 @@
+package com.minecraftdimensions.bungeesuite.managers;
+
+/**
+ * User: Bloodsplat
+ * Date: 5/09/13
+ */
+public class Affix {
+    boolean type;
+    String group;
+    String affix;
+
+    public Affix( boolean b, String g, String a ) {
+        this.type = b;
+        this.group = g;
+        this.affix = a;
+    }
+
+}
